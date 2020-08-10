2d1999e0fcaa010a261948e4b3d516723e9129c5
adarshsoodan@gmail.com <adarshsoodan@gmail.com>
2020-08-10 13:24:44 +0200

    Adding dictionary support
    Dictionary can be trained using utility method
    ZstdCoder.trainDictionary()

--- src/test/java/in/neolog/beam/coder/TestZstdCoder.java
+++ src/test/java/in/neolog/beam/coder/TestZstdCoder.java
@@ -9,14 +9,21 @@ import static org.junit.Assert.assertEquals;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.beam.sdk.coders.CoderException;
 import org.apache.beam.sdk.coders.SerializableCoder;
-import org.apache.beam.vendor.grpc.v1p26p0.org.bouncycastle.util.Arrays;
+import org.apache.commons.io.IOUtils;
 import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.github.luben.zstd.Zstd;
 import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.generator.Fields;
@@ -26,7 +33,7 @@ import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 public class TestZstdCoder {
 
     @Property(shrink = false)
-    public void testWithSerializableCoder(@From(Fields.class) SampleBean bean) throws CoderException, IOException {
+    public void testWithoutDictionary(@From(Fields.class) SampleBean bean) throws CoderException, IOException {
         SerializableCoder<SampleBean> serializableCoder = SerializableCoder.of(SampleBean.class);
         ZstdCoder<SampleBean> zstdCoder = ZstdCoder.of(serializableCoder);
 
@@ -35,14 +42,13 @@ public class TestZstdCoder {
         byte[] encoded = outStream.toByteArray();
 
         ByteArrayInputStream inStream = new ByteArrayInputStream(encoded);
-
         SampleBean decoded = zstdCoder.decode(inStream);
 
         assertEquals(bean, decoded);
     }
 
     @Test
-    public void testSmallerSizeWithSerializableCoder() throws CoderException, IOException {
+    public void testCompressionReducesSize() throws CoderException, IOException {
         SerializableCoder<SampleBean> serializableCoder = SerializableCoder.of(SampleBean.class);
         ZstdCoder<SampleBean> zstdCoder = ZstdCoder.of(serializableCoder);
 
@@ -66,7 +72,7 @@ public class TestZstdCoder {
     }
 
     @Test
-    public void testLargerSizeWithSerializableCoder() throws CoderException, IOException {
+    public void testCompressionIncreasesSizeForSmallObject() throws CoderException, IOException {
         SerializableCoder<SampleBean> serializableCoder = SerializableCoder.of(SampleBean.class);
         ZstdCoder<SampleBean> zstdCoder = ZstdCoder.of(serializableCoder);
 
@@ -87,4 +93,72 @@ public class TestZstdCoder {
                 + uncompressed.length, compressed.length > uncompressed.length);
     }
 
+    @Test
+    public void testWithDictionary() throws CoderException, IOException {
+        try (InputStream wapStream = TestZstdCoder.class.getResourceAsStream("/war and peace.txt")) {
+
+            String input = new String(IOUtils.toByteArray(wapStream), StandardCharsets.UTF_8);
+            String[] lines = input.split("((?<=(\\r\\n))|(?<=(\\n)))");
+
+            int chunkSize = 1024;
+            byte[][] trainOn;
+            {
+                trainOn = Arrays.stream(lines)
+                        .collect(() -> new ArrayList<ByteArrayOutputStream>(), (arrs, line) -> {
+                            if (arrs.isEmpty()) {
+                                arrs.add(new ByteArrayOutputStream());
+                            }
+                            byte[] lineBytes = line.getBytes(StandardCharsets.UTF_8);
+                            ByteArrayOutputStream last = arrs.get(arrs.size() - 1);
+                            if (last.size() + lineBytes.length > chunkSize) {
+                                arrs.add(new ByteArrayOutputStream());
+                                last = arrs.get(arrs.size() - 1);
+                            }
+                            try {
+                                last.write(lineBytes);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }, (arrs1, arrs2) -> {
+                            arrs1.addAll(arrs2);
+                        })
+                        .stream()
+                        .map(baos -> baos.toByteArray())
+                        .collect(Collectors.toList())
+                        .toArray(new byte[][] {});
+            }
+
+            List<SampleBean> trainingSamples = new ArrayList<>();
+            for (int i = 0; i < trainOn.length / 10; i++) {
+                SampleBean b = new SampleBean();
+                b.setX(i);
+                b.setStr(new String(trainOn[i], StandardCharsets.UTF_8));
+                b.setBytes(new byte[0]);
+                trainingSamples.add(b);
+            }
+            SerializableCoder<SampleBean> innerCoder = SerializableCoder.of(SampleBean.class);
+
+            byte[] dict = ZstdCoder.trainDictionary(innerCoder, trainingSamples, 10 * 1024);
+            ZstdCoder<SampleBean> zstdCoder = ZstdCoder.of(innerCoder, dict);
+
+            List<SampleBean> expected = new ArrayList<>();
+            List<SampleBean> actual = new ArrayList<>();
+            for (int i = 0; i < lines.length; i++) {
+                SampleBean b = new SampleBean();
+                b.setX(i);
+                b.setStr(lines[i]);
+                b.setBytes(new byte[0]);
+                expected.add(b);
+
+                ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+                zstdCoder.encode(b, outStream);
+                byte[] encoded = outStream.toByteArray();
+
+                ByteArrayInputStream inStream = new ByteArrayInputStream(encoded);
+                SampleBean decoded = zstdCoder.decode(inStream);
+                actual.add(decoded);
+            }
+            assertEquals(expected, actual);
+        }
+    }
 }
